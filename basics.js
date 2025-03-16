5fde2a01f98ea79b2d46072886dddef795e2aa49
Hamna1168 <hamnac02@gmail.com>
2025-03-16 17:11:23 +0500

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,13 @@
+
+
+const my list=document.getElementById("my list");
+const fruits=["apple","banana","cherry",]
+{
+  for(let i=0;i<fruits.lehgth;i++){
+continue;
+  }
+const listItem=
+document.create Element("LI");
+listItem.textcontent=fruits;
+MediaQueryList.appendchild(list item)
+}
